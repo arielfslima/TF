c956f9205e9878919cefd34f49a0fd4ae045cd35
arielfslima <30813672+arielfslima@users.noreply.github.com>
2017-11-27 21:49:53 -0200

    Add files via upload

--- ProjetoFinal/settings.py
+++ ProjetoFinal/settings.py
@@ -77,9 +77,12 @@ WSGI_APPLICATION = 'ProjetoFinal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'sql_server.pyodbc',
+        'NAME': 'impactadb',
+        'USER': 'impactadb',
+        'PASSWORD':'qwerty123@',
+        'HOST': 'impatadb.database.windows.net',
+    },
 }
 
 
